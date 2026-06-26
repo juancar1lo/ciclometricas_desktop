b410ce7a0d4fb2d942e927e705098c86b7bfbe46
juancar1lo <50251927+juancar1lo@users.noreply.github.com>
2026-06-26 20:33:29 +0200

    Delete ui directory

--- ui/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from .import_view import ImportView
-from .settings_view import SettingsView
-from .activities_view import ActivitiesView
-
-__all__ = ["ImportView", "SettingsView", "ActivitiesView"]
--- ui/widgets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .stat_card import StatCard
-from .alert_banner import AlertBanner
-
-__all__ = ["StatCard", "AlertBanner"]
